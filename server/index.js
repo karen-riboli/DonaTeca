acf2dfa6b90aaad3cee2dd81706fef97bb54fd3a
karen-riboli <kr.oliveira14@gmail.com>
2026-09-01 14:35:27 -0300

    adapt to use Render

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
